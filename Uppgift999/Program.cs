950d2ce1dd76782c9d179cc37b91ed9e20732735
EuronLubeniqi <eulu07001@utb.vaxjo.se>
2024-12-17 14:11:58 +0100

    Add project files.

---
+++ Uppgift999/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift999
+{
+    class Program
+    {
+        static void Main()
+        {
+            int rows = 4; 
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++) 
+                {
+                    Console.Write("*"); 
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
